849714282f39844f08eaf529251b896cae5ba238
N04h <contact@n04h.jp>
2021-02-22 14:46:27 +0900

    ✨ パッケージソース追加

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+function hello(name) {
+    console.log('Hello', name);
+}
+exports.hello = hello;
